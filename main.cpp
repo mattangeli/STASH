9c3438565ef52227655dd5ac7811817ab5ef8ea7
Matteo Seclì <secli.matteo@gmail.com>
2019-05-24 16:05:22 +0200

    Implementation of idNext() and processing_time()
    - The taskBlock can now go to multiple blocks (I've removed the check for
      probsOut to be of length 1)
    - idNext() is implemented for taskBlock as well (it just returns idsOut)
    - Small redefinitions of some parameters just to avoid possible misuse
    - processing_time() now returns either a constant value or a value according
      to a truncated normal distribution; the constructor was changed accordingly,
      and by default (i.e., with no such time parameters) it constructs a block
      with 0 processing time.
    - Extra tests in main.cpp, only with DEBUG flag.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #ifdef DEBUG
     #include <iomanip>
+    #include <algorithm>
 #endif
 
 
@@ -67,7 +68,7 @@ int main()
     /* Initialize Blocks */
     int idTest;
     for (idTest = 0; idTest < nblocks; ++idTest) {
-		blocksVector.emplace_back(new taskBlock(idTest, vector<int>(1,idTest+1), vector<float>(1,1), wlkrestest));
+        blocksVector.emplace_back(new taskBlock(idTest, vector<int>(1,idTest+1), vector<float>(1,1), wlkrestest, 0, vector<float>(1,0)));
 		
     }
     blocksVector.emplace_back(new taskBlock(idTest, vector<int>(1,nblocks), vector<float>(1,1), wlkrestest));
@@ -120,6 +121,9 @@ int main()
 
     #ifdef DEBUG
         // Xor Block Test
+        cout << endl << "=============================================" << endl
+             << "    DEBUG::TEST:  xorBlock test." << endl
+             << "=============================================" << endl;
         int outcomes[16]={};
         int nextId; // from 0 to 15
         const int nstars = 100;
@@ -133,6 +137,93 @@ int main()
         cout << "    DEBUG::TEST:  ID    P (%)" << endl;
         for (int i=0; i < 16; i++) {
             cout << setfill(' ')  << "    DEBUG::TEST:  " << setw(2) << i << "    " << setw(3) << outcomes[i]*nstars/nrolls << "    " << string(outcomes[i]*nstars/nrolls,'*') << endl;
+        }
+    #endif
+
+    #ifdef DEBUG
+        // Task Block Time Test #1
+        cout << endl << "=============================================" << endl
+             << "    DEBUG::TEST:  taskBlock time test #1." << endl
+             << "=============================================" << endl;
+        int T1bins = 50;
+        vector<int> T1outcomes(T1bins,0);
+        const float T1mean = 4.0;
+        const float T1std  = 0.5;
+        const float T1min  = 0.0;
+        const float T1max  = 10.0;
+        float T1processingTime;
+        const int T1nstars = 100;
+        const int T1nrolls = 100000;
+        taskBlock myTaskBlockDistr = taskBlock(25, vector<int>{9,5,13,15}, vector<float>(4,1.0), wlkrestest, 1, vector<float>{T1mean,T1std,T1min,T1max});
+        for (int i=0; i < T1nrolls; i++) {
+            T1processingTime = myTaskBlockDistr.processing_time();
+            ++T1outcomes.at(round(T1processingTime*(float)T1bins/(T1max-T1min)));
+        }
+        cout << "    DEBUG::TEST:  TIME  P (exp)  P (exact)" << endl;
+        for (int i=0; i < T1bins; i++) {
+            cout << setfill(' ')  << "    DEBUG::TEST:  "
+                 << setw(4) << ((T1max-T1min)/(float)T1bins)*((float)i) << "      "
+                 << setw(3) << round((float)T1outcomes.at(i)*(float)nstars/(float)nrolls) << "        "
+                 << setw(3) << round(exp(-0.5*pow((((T1max-T1min)/(float)T1bins)*((float)i)-T1mean)/T1std,2.0))*((float)*max_element(T1outcomes.begin(),T1outcomes.end()))*(float)nstars/(float)nrolls) << "      "
+                 << string(T1outcomes.at(i)*T1nstars/T1nrolls,'*') << endl;
+        }
+    #endif
+
+    #ifdef DEBUG
+        // Task Block Time Test #2
+        cout << endl << "=============================================" << endl
+             << "    DEBUG::TEST:  taskBlock time test #2." << endl
+             << "=============================================" << endl;
+        int T2bins = 20;
+        vector<int> T2outcomes(T2bins,0);
+        const float T2mean = 4.0;
+        const float T2std  = 1E-15;
+        const float T2min  = 0.0;
+        const float T2max  = 8.0;
+        float T2processingTime;
+        const int T2nstars = 10;
+        const int T2nrolls = 100000;
+        taskBlock myTaskBlockConst = taskBlock(26, vector<int>{9,5,13,15}, vector<float>(4,1.0), wlkrestest, 0, vector<float>{T2mean});
+        for (int i=0; i < T2nrolls; i++) {
+            T2processingTime = myTaskBlockConst.processing_time();
+            ++T2outcomes.at(round(T2processingTime*(float)T2bins/(T2max-T2min)));
+        }
+        cout << "    DEBUG::TEST:  TIME  P (exp)  P (exact)" << endl;
+        for (int i=0; i < T2bins; i++) {
+            cout << setfill(' ')  << "    DEBUG::TEST:  "
+                 << setw(4) << ((T2max-T2min)/(float)T2bins)*((float)i) << "      "
+                 << setw(3) << round((float)T2outcomes.at(i)*(float)nstars/(float)nrolls) << "        "
+                 << setw(3) << round(exp(-0.5*pow((((T2max-T2min)/(float)T2bins)*((float)i)-T2mean)/T2std,2.0))*((float)*max_element(T2outcomes.begin(),T2outcomes.end()))*(float)nstars/(float)nrolls) << "      "
+                 << string(T2outcomes.at(i)*T2nstars/T2nrolls,'*') << endl;
+        }
+    #endif
+
+    #ifdef DEBUG
+        // Task Block Time Test #3
+        cout << endl << "=============================================" << endl
+             << "    DEBUG::TEST:  taskBlock time test #3." << endl
+             << "=============================================" << endl;
+        int T3bins = 20;
+        vector<int> T3outcomes(T3bins,0);
+        const float T3mean = 0.0;
+        const float T3std  = 1E-15;
+        const float T3min  = 0.0;
+        const float T3max  = 8.0;
+        float T3processingTime;
+        const int T3nstars = 10;
+        const int T3nrolls = 100000;
+        taskBlock myTaskBlockConst0 = taskBlock(26, vector<int>{9,5,13,15}, vector<float>(4,1.0), wlkrestest);
+        for (int i=0; i < T3nrolls; i++) {
+            T3processingTime = myTaskBlockConst0.processing_time();
+            ++T3outcomes.at(round(T3processingTime*(float)T3bins/(T3max-T3min)));
+        }
+        cout << "    DEBUG::TEST:  TIME  P (exp)  P (exact)" << endl;
+        for (int i=0; i < T3bins; i++) {
+            cout << setfill(' ')  << "    DEBUG::TEST:  "
+                 << setw(4) << ((T3max-T3min)/(float)T3bins)*((float)i) << "      "
+                 << setw(3) << round((float)T3outcomes.at(i)*(float)nstars/(float)nrolls) << "        "
+                 << setw(3) << round(exp(-0.5*pow((((T3max-T3min)/(float)T3bins)*((float)i)-T3mean)/T3std,2.0))*((float)*max_element(T3outcomes.begin(),T3outcomes.end()))*(float)nstars/(float)nrolls) << "      "
+                 << string(T3outcomes.at(i)*T3nstars/T3nrolls,'*') << endl;
         }
     #endif
 
